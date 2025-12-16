a656cbb75c96c4c09131b13822c980aa77c5c1bf
CTEPXMGN <fuser-mgn@ya.ru>
2025-12-16 18:55:51 +0500

    Добавил страницу логина

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCard } from "./common-ui/profile-card/profile-card";
-import { Profile } from './data/services/profile.service';
-import { ProfileInterface } from './data/interfaces/profile.interface';
 import { JsonPipe } from '@angular/common';
 
 @Component({
@@ -12,14 +10,5 @@ import { JsonPipe } from '@angular/common';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('ng-lesson');
 
-  profileService = inject(Profile);
-  profiles : ProfileInterface[] = [];
-
-  constructor() {
-    this.profileService.getTestAccounts().subscribe((data: ProfileInterface[]) => {
-      this.profiles = data;
-    });
-  }
 }
--- src/app/pages/search-page/search-page.ts
+++ src/app/pages/search-page/search-page.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ProfileCard } from "../../common-ui/profile-card/profile-card";
+import { Profile } from '../../data/services/profile.service';
+import { ProfileInterface } from '../../data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [ProfileCard],
   templateUrl: './search-page.html',
   styleUrl: './search-page.scss',
 })
 export class SearchPage {
+  protected readonly title = signal('ng-lesson');
 
+  profileService = inject(Profile);
+  profiles : ProfileInterface[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((data: ProfileInterface[]) => {
+      this.profiles = data;
+    });
+  }
 }

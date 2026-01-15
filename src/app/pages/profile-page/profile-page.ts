d9a5d45534e2c8ddb0a1902639fe952ad46701af
CTEPXMGN <fuser-mgn@ya.ru>
2026-01-15 15:44:12 +0500

    Делаю страницу профиля

--- src/app/pages/profile-page/profile-page.ts
+++ src/app/pages/profile-page/profile-page.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProfileHeader } from '../../common-ui/profile-header/profile-header';
+import { ActivatedRoute } from '@angular/router';
+import { Profile } from '../../data/services/profile.service';
+import { switchMap } from 'rxjs';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [],
+  imports: [ProfileHeader, AsyncPipe],
   templateUrl: './profile-page.html',
   styleUrl: './profile-page.scss',
 })
 export class ProfilePage {
+  profileService = inject(Profile);
+  route = inject(ActivatedRoute);
 
+  me$ = toObservable(this.profileService.me);
+
+  profile$ = this.route.params.pipe(
+    switchMap(({ id }) => {
+      if (id === 'me') return this.me$;
+      return this.profileService.getAccount(id);
+    })
+  );
 }

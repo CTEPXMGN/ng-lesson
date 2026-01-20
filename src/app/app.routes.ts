012cdb406cb020035adf6efdd2435c533ead6a9e
CTEPXMGN <fuser-mgn@ya.ru>
2026-01-20 14:12:51 +0500

    Редактирование профиля

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
 import { SearchPage } from './pages/search-page/search-page';
 import { ProfilePage } from './pages/profile-page/profile-page';
+import { SettingsPage } from './pages/settings-page/settings-page';
 import { Layout } from './common-ui/layout/layout';
 import { canActivateAuth } from './auth/access.guard';
 
@@ -12,6 +13,7 @@ export const routes: Routes = [
     children: [
       { path: '', component: SearchPage },
       { path: 'profile/:id', component: ProfilePage },
+      { path: 'settings', component: SettingsPage },
     ],
     canActivate: [canActivateAuth],
   },
